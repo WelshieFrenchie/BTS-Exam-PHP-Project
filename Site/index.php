10da2ef434005ed2e128b7df307e5294c0023246
WelshieFrenchie <156076125+WelshieFrenchie@users.noreply.github.com>
2024-01-09 13:38:31 +0100

    Initial upload with Bootstrap framework

---
+++ Site/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<meta charset='utf-8'>
+    	<meta name='viewport' content='width=device-width, initial-scale=1'>
+    	<title>Accueil Salle de sport</title>
+    	<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' rel='stylesheet'>
+    </head>
+    <body>
+    	<h1>Hello World!</h1>
+    </body>
+</html>
